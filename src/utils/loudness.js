443530c2f57557be1486bd37086c8c5c8d5e242a
TeeTyJunGz <vasayos.tsr@gmail.com>
2026-09-23 14:42:31 +0700

    Adjusted loudness LUFS to -12

--- src/utils/loudness.js
+++ src/utils/loudness.js
@@ -22,7 +22,7 @@ const Logger = require("./logger");
 
 const CACHE_FILE = path.join(__dirname, "../../data/loudness-cache.json");
 const COOKIES_FILE = path.join(__dirname, "../../youtube-cookies.txt"); // Optional: export cookies from browser
-const TARGET_LUFS = -10;
+const TARGET_LUFS = -12;
 const MIN_GAIN = 0.3;
 const MAX_GAIN = 2.5;
 const SPOTIFY_FALLBACK_GAIN = 0.9;
